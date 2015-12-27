fa888c6a8fdc43b5800f41581f3f1f8f904f356c
liangzhuo <zyff032@126.com>
2015-12-27 15:10:26 +0800

    develop view

--- chapter2/src/main/java/com/liang/chapter2/controller/CustomerServlet.java
+++ chapter2/src/main/java/com/liang/chapter2/controller/CustomerServlet.java
@@ -1,9 +1,34 @@
 package com.liang.chapter2.controller;
 
+import com.liang.chapter2.model.Customer;
+import com.liang.chapter2.service.CustomerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by liangz on 2015/12/25.
+ * 进入客户列表
  */
+@WebServlet("/customer")
 public class CustomerServlet extends HttpServlet{
+
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
+    }
 }
