67362c3925c91f5fa37e718ef3a12826b1ece5a0
liangzhuo <zyff032@126.com>
2015-12-27 15:15:19 +0800

    sql file

---
+++ chapter2/src/test/resources/sql/demo_test.sql
@@ -0,0 +1,36 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50546
+Source Host           : localhost:3306
+Source Database       : demo_test
+
+Target Server Type    : MYSQL
+Target Server Version : 50546
+File Encoding         : 65001
+
+Date: 2015-12-27 15:12:34
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `customer`
+-- ----------------------------
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(255) NOT NULL COMMENT '客户名称',
+  `contact` varchar(255) NOT NULL COMMENT '联系人',
+  `telephone` varchar(255) DEFAULT NULL COMMENT '电话号码',
+  `email` varchar(255) DEFAULT NULL COMMENT '邮箱地址',
+  `remark` text COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of customer
+-- ----------------------------
+INSERT INTO `customer` VALUES ('1', 'customer1', 'Jack', '13512345678', 'jack@email.com', null);
+INSERT INTO `customer` VALUES ('2', 'customer2', 'Rose', '13623456789', 'rose@email.com', null);
